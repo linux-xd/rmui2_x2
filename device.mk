64d6b0af9f4abb342281b5f4877fa0120fbe5e40
kaderbava <ksbava7325@gmail.com>
2025-01-14 13:25:34 +0000

    RMX1921: Update system blobs from LA.QSSI.15.0.r1-12900-qssi.0
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- device.mk
+++ device.mk
@@ -554,7 +554,7 @@ PRODUCT_COPY_FILES += \
 
 # WiFi Display
 PRODUCT_PACKAGES += \
-    android.media.audio.common.types-V2-cpp \
+    android.media.audio.common.types-V3-cpp \
     libnl \
     libpng.vendor \
     libprocessgroup.vendor \
